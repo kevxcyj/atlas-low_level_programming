5a18b094df254e1f71aee7a9202c5d28643f4993
kevxcyj <kevcyjunior@gmail.com>
2024-01-23 04:19:02 +0000

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -2,12 +2,12 @@
 
 int main(void)
 {
-	char main.h[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+	char main[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
 	unsigned int c;
 
-	for (c = 0; c < sizeof(main.h); c++)
+	for (c = 0; c < sizeof(main); c++)
 	{
-		_putchar(main.h[c]);
+		_putchar(main[c]);
 	}
 _putchar('\n');
 return (0);

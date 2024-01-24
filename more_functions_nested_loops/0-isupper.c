8ad335a3d4354bcb41e368f20086a925dffc9fdb
root <kevxcyj>
2024-01-24 23:51:14 +0000

    Trying number 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -8,15 +8,12 @@
  * Return
  **/
 
-int main(void)
+int _isupper(int c)
 {
-	char c;
-
-	c = 'A';
-	printf("%c: %d\n, c, _isupper(c));
-	c = 'a';
-	printf("%c: %d\n, c, _isupper(c));
-	return (0);
-}	
+	if (c >= 'A' && c =< 'Z')
+		return (1)
+	else	
+		return (0)
+}			
 
 

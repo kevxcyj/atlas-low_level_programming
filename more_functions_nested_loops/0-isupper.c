ce5e8c92d323dde158e8734aa80f8c19fe564141
root <kevxcyj>
2024-01-24 23:37:23 +0000

    This is the first commit 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ *
+ * Main - Main function 
+ *
+ * Return
+ **/
+
+int main(void)
+{
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n, c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n, c, _isupper(c));
+	return (0);
+}	
+
+

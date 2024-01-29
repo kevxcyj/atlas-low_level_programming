5947aad49eef94a20e6a0518a4dc2673fca2ed79
kevxcyj <kevcyjunior@gmail.com>
2024-01-29 20:28:24 +0000

    this

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - Prints numbers 0 - 9 followed by a new line
+ *
+ * Return: 0
+ **/
+
+void print_number(void)
+{
+	int i = 48;
+	for (i = 48; i < 58; i++)
+	{
+		printf(i);
+		return (10);
+	}
+}	
---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print most numbers - Prints the numbers followed by a new line
+ *
+ * Return: 0
+ **/
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	for (; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+			else
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}	

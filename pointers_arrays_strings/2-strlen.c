832b7f6bd571a6d0496cab39d05a99902c923c93
kevxcyj <kevcyjunior@gmail.com>
2024-01-29 22:39:36 +0000

    this is 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - Returns the length of the string
+ *
+ * @s: string
+ *
+ * Return: 0
+ **/
+
+int _strlen(char *s)
+{
+	int count, inc;
+	inc = 0;
+		for (count =0; s[count] != '\0'; count++)
+	inc++;
+return (inc);
+}

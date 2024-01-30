dabce0efbbe9e29a2195ce0ec2c1e3a6f2514bdc
kevxcyj <kevcyjunior@gmail.com>
2024-01-30 01:14:52 +0000

    this is 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print rev - Prints string in reverse
+ * @s : String
+ *
+ * Return: 0
+ **/
+
+void print_rev(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+	{ }
+for (c = c - 1; c >= 0; c--)
+		putchar(s[c]);
+putchar('\n');
+}
+	
+

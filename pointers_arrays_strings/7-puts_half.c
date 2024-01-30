fca97cd8de21f79e53c274ecf1e3e1ddae1bc8d6
kevxcyj <kevcyjunior@gmail.com>
2024-01-30 01:46:06 +0000

    8

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+void puts_half(char *str)
+{
+	int k, b;
+	char m;
+
+	for (k = 0; str[k] != 0; k++)
+	{}
+if (k % 2 == 0)
+{
+	for ((b = k / 2); str[b] != 0; b++)
+	{
+	
+		m = str[b];
+		putchar(m);
+	}
+}
+else
+{
+	for ((b = (k - 1) / 2); str[b] != 0; b++)
+	{
+		m = str[b];
+		putchar(m);
+	}
+}
+putchar('\n');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ void _puts(char *str);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void print_rev(char *s);
+void puts_half(char *str);
 
 #endif

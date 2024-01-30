5c101e175ca7cde1b06e783f4d5c8870f6e2dadc
kevxcyj <kevcyjunior@gmail.com>
2024-01-30 02:30:01 +0000

    number 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,11 +1,20 @@
 #include "main.h"
+/**
+ * point_array - Prints elements of array of integers
+ * @a: parameter
+ * @n: parameter
+ *
+ * Return: 0
+ **/
+
+
 
 void print_array(int *a, int n)
 {
 	int inc;
-for (inc = 0; inc < n ; inc++)
+	for (inc = 0; inc < n ; inc++)
 {
-		if (inc != n - 1)
+if (inc != n - 1)
 printf("%d, ", a[inc]);
 	else
 		printf("%d", a[inc]);

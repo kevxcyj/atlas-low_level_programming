21c6d82802255261ce8ca3c1ffbe24791ad37da8
kevxcyj <kevcyjunior@gmail.com>
2024-02-05 23:34:29 +0000

    number 4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * strpbrk - Searches a string for any of a set of a bytes
+ * @s: parameter
+ * @accept: parameter
+ *
+ * Return: 0
+ **/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int a;
+
+	while (*s)
+
+	{
+		for (a = 0; accept[a]; a++)
+		{
+		if (*s == accept[a])
+			{
+			return (s);
+			}
+		}
+		s++;
+	}
+return (0);
+}

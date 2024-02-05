666e1db80ec729e7307951c356c57be50af5ae23
kevxcyj <kevcyjunior@gmail.com>
2024-02-05 19:12:44 +0000

    number 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * cap_string - Capitalizes words in a string
+ * @str: String
+ *
+ * Return: 0
+ **/
+
+char *cap_string(char *str)
+{
+	int k = 0;
+
+	while (str[k])
+	{
+		while (!(str[k] >= 'a' && str[k] <= 'z'))
+			k++;
+
+		if (str[k - 1] == ' ' ||
+		    str[k - 1] == '\t' ||
+		    str[k - 1] == '\n' ||
+		    str[k - 1] == ',' ||
+		    str[k - 1] == ';' ||
+		    str[k - 1] == '.' ||
+		    str[k - 1] == '!' ||
+		    str[k - 1] == '?' ||
+		    str[k - 1] == '"' ||
+		    str[k - 1] == '(' ||
+		    str[k - 1] == ')' ||
+		    str[k - 1] == '{' ||
+		    str[k - 1] == '}' ||
+		    k == 0)
+		str[k] -= 32;
+
+	k++;
+	}
+
+return (0);
+}

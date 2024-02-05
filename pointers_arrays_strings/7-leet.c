42e6a92b7690725983d4c93b716d3207e05fe3bb
kevxcyj <kevcyjunior@gmail.com>
2024-02-05 03:09:30 +0000

    number 7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+char *leet(char *n)
+{
+	int b = 0;
+
+	for (b = 0; n[b] != '\0'; b++)
+	{
+		while (n[b] == 'A' || n[b] == 'a')
+			n[b] = '4';
+
+		while (n[b] == 'E' || n[b] == 'e')
+			n[b] = '3';
+
+		while (n[b] == 'O' || n[b] == 'o')
+			n[b] = '0';
+
+		while (n[b] == 'T' || n[b] == 't')
+			n[b] = '7';
+
+		while (n[b] == 'L' || n[b] == 'l')
+			n[b] = '1';
+	}
+
+return (0);
+}

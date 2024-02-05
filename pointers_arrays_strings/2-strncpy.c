adae9d80875086b9a7ed050aef274e1938bb06c8
kevxcyj <kevcyjunior@gmail.com>
2024-02-05 19:21:14 +0000

    fixing 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -15,7 +15,7 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (a = 0; a < n && src[a] != '\0'; a++)
 		dest[a] = src[a];
-	for (; n < a; a--)
+	for (; n > a; a++)
 		dest[a] = '\0';
 
 return (dest);

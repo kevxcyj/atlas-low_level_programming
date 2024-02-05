ae12fbd5d747e40addb2e422b95c07494d04b18c
kevxcyj <kevcyjunior@gmail.com>
2024-02-05 22:47:21 +0000

    number 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strchr - Locates a character in a string
+ * @s: parameter
+ * @c: parameter
+ *
+ * Return: 0
+ **/
+
+char *_strchr(char *s, char c)
+{
+	int a = 0;
+
+	for (a = 0; (s[a] != c) && (s[a] != '\0'); a++);
+
+	if (s[a] == c)
+		return (s + a);
+	else
+
+return (0);
+
+}

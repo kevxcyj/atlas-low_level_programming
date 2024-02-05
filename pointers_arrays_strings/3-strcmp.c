cf35f6356cf8d47f116e84bda89340770c6ef30b
kevxcyj <kevcyjunior@gmail.com>
2024-02-05 01:41:08 +0000

    fixing 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,12 +12,10 @@ int _strcmp(char *s1, char *s2)
 {
 	int a = 0;
 
-	for(a = 0; s1[a] != '\0' || s2[a] != '\0'; a++)
+	for (a = 0; s1[a] != '\0' || s2[a] != '\0'; a++)
 {
 	if (s2[a] != s1[a])
 		return (s1[a] - s2[a]);
 	}
 return (0);
 }
-
-	

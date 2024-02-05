9060fee14196020bb346e688af44937f650e25cc
kevxcyj <kevcyjunior@gmail.com>
2024-02-05 23:01:04 +0000

    number 3

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -12,7 +12,8 @@ char *_strchr(char *s, char c)
 {
 	int a = 0;
 
-	for (a = 0; (s[a] != c) && (s[a] != '\0'); a++);
+	for (a = 0; (s[a] != c) && (s[a] != '\0'); a++)
+		;
 
 	if (s[a] == c)
 		return (s + a);
---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <string.h>
+/**
+ * strspn - Gets the length of a prefix substring
+ * @s: parameter
+ * @accept: parameter
+ *
+ * Return: 0
+ **/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int c = 0;
+	int a;
+	int b;
+	int d;
+
+	b = 0;
+	while (s[b] != '\0')
+	{
+		a = 0;
+		for (d = 0; accept[d] != '\0'; d++)
+		{
+	if (s[a] == accept[d])
+	a = 0;
+		}
+	d = 0;
+	if (a == 0)
+		break;
+
+	c++;
+	a++;
+	}
+return (a);
+
+}
+
+	

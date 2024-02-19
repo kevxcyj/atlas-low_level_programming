b4ee2f30e58553d64d4174433ba84a45da3edaee
kevxcyj <kevcyjunior@gmail.com>
2024-02-19 11:08:36 +0000

    fixing 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -11,18 +11,17 @@
 char *create_array(unsigned int size, char c)
 {
 	char *k;
-	int b;
+	unsigned int b;
 
 	if (size == 0)
 		return (NULL);
 
-	k = (char)malloc(sizeof(char) * size);
+	k = malloc(sizeof(char) * size);
 
 	if (k == NULL)
 		return (NULL);
 
 	for (b = 0; b < size; b++)
 		k[b] = c;
-
 return(k);
 }

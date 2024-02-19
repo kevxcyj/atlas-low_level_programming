61010f1a05cabddd14b1b8cd803203476fa79b8f
kevxcyj <kevcyjunior@gmail.com>
2024-02-19 11:53:25 +0000

    number 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -9,19 +9,17 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int a = 0:
-	int b = 0;
-	int c;
+	int a = 0; int b = 0; int c;
 	char *d;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 ==  NULL)
 		s2 = "";
-	
+
 	for (c = 0; s1[c] != '\0'; c++)
 		a++;
-	for (c = 0; s2[i] != '\0'; c++)
+	for (c = 0; s2[c] != '\0'; c++)
 		b++;
 
 	d = malloc(sizeof(char) * (a + b) + 1);
@@ -29,4 +27,10 @@ char *str_concat(char *s1, char *s2)
 	if (d == NULL)
 		return (NULL);
 
+	for (c = 0; s1[c] != '\0'; c++)
+		d[c] = s1[c];
+	for (c = 0; s2[c] != '\0'; c++)
+		d[c] = s2[c];
+
+return (d);
 }

099a486bef8dfa80e0b62f68d6a363411ff28200
kevxcyj <kevcyjunior@gmail.com>
2024-02-19 23:15:21 +0000

    moving code over in 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-pointer = calloc(nmemb, size);
+	pointer = calloc(nmemb, size);
 
 	if (pointer == NULL)
 		return (NULL);

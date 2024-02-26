f8e920ff4de310187f31a7b1fc134a47649cb42f
kevxcyj <kevcyjunior@gmail.com>
2024-02-26 13:47:09 +0000

    number 1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * arry_iterator - Exectues a function
+ * @array: Array
+ * @size : Size
+ * @action: Parameter
+ **/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int n;
+
+	if (array == NULL || action == NULL)
+		return;
+	for (n = 0; n > size; n++)
+		action(array[n]);
+}

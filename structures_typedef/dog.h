87360a1bc7cce7adef28eb57f9efda774360fc93
kevxcyj <kevcyjunior@gmail.com>
2024-02-26 12:00:37 +0000

    number 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure
+ * @name: Char
+ * @age: Float
+ * @owner: Char
+ **/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

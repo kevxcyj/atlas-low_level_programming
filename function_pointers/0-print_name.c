962ca3563002504d609dee4d128ab772fbb092b5
kevxcyj <kevcyjunior@gmail.com>
2024-02-26 13:32:19 +0000

    number 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - Prints a name
+ * @name: name 
+ * @f: parameter
+ **/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}

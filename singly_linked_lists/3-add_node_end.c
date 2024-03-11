c6c443c3aae93772aeca184820ef03d9c52531a9
kevxcyj <kevcyjunior@gmail.com>
2024-03-11 11:32:15 +0000

    number 3

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,61 @@
+#include "lists.h"
+
+/**
+ * add_node_end - Adds new node to list
+ * @head: Pointer
+ * @str: String
+ *
+ * Return: Pointer
+ **/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *list, *new;
+
+	if (str == NULL)
+		return (NULL);
+
+	list = malloc(sizeof(list_t));
+
+	if (list == NULL)
+		return (NULL);
+
+	list->str = strdup(str);
+
+	if (list->str == NULL)
+	{
+		free(list);
+		return (NULL);
+	}
+
+	list->len = _strlen(list->str);
+	list->next = NULL;
+
+	if(*head == NULL)
+	{
+		*head = list;
+		return (list);
+	}
+	new = *head;
+	while (new->next)
+		new = new->next;
+	new->next = list;
+
+return (list);
+}
+
+/**
+ * _strlen - Returns lenght of str
+ * @str: string
+ *
+ * Return: lenght
+ **/
+
+unsigned int _strlen(char *str)
+{
+	unsigned int i;
+
+	for (i = 0; str[i]; i++)
+		;
+return (i);
+}
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -25,6 +25,7 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 int _strlen(const char *s);
+unsigned int _strlen(char *str);
 
 
 

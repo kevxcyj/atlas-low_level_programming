c55a911e5e3717926c113d55fc1426392acd0485
kevxcyj <kevcyjunior@gmail.com>
2024-03-12 02:06:47 +0000

    fixed betty on 3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -31,7 +31,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list->len = _strlen(list->str);
 	list->next = NULL;
 
-	if(*head == NULL)
+	if (*head == NULL)
 	{
 		*head = list;
 		return (list);

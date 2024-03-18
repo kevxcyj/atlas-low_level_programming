b3a2fe709aad548ff942d6d68236b6e219c3360e
kevxcyj <kevcyjunior@gmail.com>
2024-03-18 17:53:54 +0000

    Added number 1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * dlistint_len - Returns the # of elements
+ * @h: Head
+ *
+ * Return: # of nodes
+*/
+
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i = 0;
+
+	if (h == NULL)
+		return (i);
+
+		while (h->prev != NULL)
+			h = h->prev;
+
+		while (h != NULL)
+		{
+			i++;
+			h = h->next;
+		}
+
+return (i);
+}

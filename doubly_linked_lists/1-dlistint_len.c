471dc8643aab8e7381e8c6535cef7b112c56b65b
kevxcyj <kevcyjunior@gmail.com>
2024-03-18 17:57:20 +0000

    Change while loop

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -14,14 +14,14 @@ size_t dlistint_len(const dlistint_t *h)
 	if (h == NULL)
 		return (i);
 
-		while (h->prev != NULL)
-			h = h->prev;
+	while (h->prev != NULL)
+		h = h->prev;
 
-		while (h != NULL)
-		{
-			i++;
-			h = h->next;
-		}
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
 
 return (i);
 }

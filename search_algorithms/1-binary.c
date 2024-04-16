0296d3af812f949a157227744f6603edbc7cd60c
kevxcyj <kevcyjunior@gmail.com>
2024-04-16 18:42:13 +0000

    Lowercase a

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -19,7 +19,7 @@ int binary_search(int *array, size_t size, int value)
 
 	while (l <= r)
 	{
-		printf("Searching in Array: ");
+		printf("Searching in array: ");
 		for (i = l; i < r; i++)
 			printf("%d, ", array[i]);
 		printf("%d\n", array[i]);
